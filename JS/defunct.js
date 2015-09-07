a088be0907ff8cd743e2a3f81ea3552f2990de06
jfouts1979 <Fouts.John@gmail.com>
2015-09-07 11:01:02 -0400

    Changed Scrolling Function
    Changed scrolling function from JavaScript to jQuery function utilizing
    code borrowed from site bookmarked.

--- JS/defunct.js
+++ JS/defunct.js
@@ -1,56 +1,127 @@
-(function (window, document, undefined) {
-	
-	var spaceinterval = 10;
-    var timeinterval = 50; // `speed`
-    var max;
-    
-	var firstrun = true;
-     // Interval function:
-    
-	var gallery = function() {
-      var elem = document.getElementById("marquee-container");
-      if (elem && elem.clientWidth < elem.scrollWidth)  {
-       if (firstrun) {
-        max = elem.scrollWidth;
-        // Clone the children of the container until the
-        // scrollWidth is at least twice as large as max.
-        while (elem.scrollWidth < max * 2) {
-        
-		 var length = elem.children.length;
-         
-		 for (var i = 0; i < length; ++i) {
-         	 var child = elem.children[i].cloneNode(true);
-			 child.setAttribute("id", "marquee-container");
-			 elem.appendChild(child);
-			 
-         }
-         break;
-        }
-        firstrun = false;
-       }
-       if (elem.scrollLeft >= max) {
-        elem.scrollLeft -= max;
-       } else {
-        elem.scrollLeft += spaceinterval;
-       }
-      }
-     };
-     window.setInterval(gallery, timeinterval);
-    })(window, document);
+//<script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
+//<script type="text/javascript">
+    $(function(){
+        var scroller = $('#scroller div.innerScrollArea');
+        var scrollerContent = scroller.children('ul');
+        scrollerContent.children().clone().appendTo(scrollerContent);
+        var curX = 0;
+        scrollerContent.children().each(function(){
+            var $this = $(this);
+            $this.css('left', curX);
+            curX += $this.outerWidth(true);
+        });
+        var fullW = curX / 2;
+        var viewportW = scroller.width();
 
-function retNumVal(str2Num) {
-	var numberPattern = /\d+/g;
-	return str2Num.match(numberPattern);
-}
+        // Scrolling speed management
+        var controller = {curSpeed:0, fullSpeed:2};
+        var $controller = $(controller);
+        var tweenToNewSpeed = function(newSpeed, duration)
+        {
+            if (duration === undefined)
+                duration = 600;
+            $controller.stop(true).animate({curSpeed:newSpeed}, duration);
+        };
 
-//function setImgSize () {
+        // Pause on hover
+        scroller.hover(function(){
+            tweenToNewSpeed(0);
+        }, function(){
+            tweenToNewSpeed(controller.fullSpeed);
+        });
+
+        // Scrolling management; start the automatical scrolling
+        var doScroll = function()
+        {
+            var curX = scroller.scrollLeft();
+            var newX = curX + controller.curSpeed;
+            if (newX > fullW*2 - viewportW)
+                newX -= fullW;
+            scroller.scrollLeft(newX);
+        };
+        setInterval(doScroll, 25);
+        tweenToNewSpeed(controller.fullSpeed);
+    });
+//</script>
+
 //
-//var yourImg = document.getElementById('image1');
+//(function (window, document) {
+//	
+//	var spaceinterval = 100;
+//    var timeinterval = 150; // `speed`
+//    var max;
+//    
+//	var firstrun = true;
+//     // Interval function:
+//    
+//	var gallery = function() {
+//      var elem = document.getElementById("marquee-container");
+//      if (elem && elem.clientWidth < elem.scrollWidth)  {
+//       if (firstrun) {
+//        max = elem.scrollWidth;
+//        // Clone the children of the container until the
+//        // scrollWidth is at least twice as large as max.
+//        while (elem.scrollWidth < max * 2) {
+//        
+//		 var length = elem.children.length;
+//         
+//		 for (var i = 0; i < length; ++i) {
+//         	 var child = elem.children[i].cloneNode(true);
+//			 var childwithbadbehavior =  elem.children[i];
+//			 child.setAttribute("id", "marquee-container");
+//		
+//			 childwithbadbehavior.style.paddingLeft = "0px";
+//			 childwithbadbehavior.style.paddingRight = "0px";
+//			 childwithbadbehavior.style.marginLeft = "0px";
+//			 childwithbadbehavior.style.marginRight = "0px";
+//			 
+////			 var margleft = childwithbadbehavior.style.marginLeft;
+////			 alert("Margin Left Image: " + i + " is " + margleft);
+////			 
+////			 var margright = childwithbadbehavior.style.marginRight;
+////			 alert("Margin Right Image: " + i + " is " + margright);
+////			 
+////			 var paddleft  
+////			 = childwithbadbehavior.style.paddingLeft;
+////			 alert("Padding Left Image: " + i + " is " + paddleft);
+////			 
+////			 var paddright = childwithbadbehavior.style.paddingRight;
+////			 alert("Padding Right Image: " + i + " is " + paddright);
+//			 
+//			 elem.appendChild(child);
+//		 }
+//			
+//			
+//         break;
+//        
+//		}
+//        
+//	   firstrun = false;
+//       
+//	   }
+//       if (elem.scrollLeft >= max) {
+//        elem.scrollLeft -= max;
+//       } else {
+//        elem.scrollLeft += spaceinterval;
+//       }
+//      }
+//     };
+//     window.setInterval(gallery, timeinterval);
+//    })(window, document);
 //
-//if(yourImg && yourImg.style) {
-//    yourImg.style.height = '200px';
-//    yourImg.style.width = '200px';
+//function retNumVal(str2Num) {
+//	var numberPattern = /\d+/g;
+//	return str2Num.match(numberPattern);
 //}
+//
+////function setImgSize () {
+////
+////var yourImg = document.getElementById('image1');
+////
+////if(yourImg && yourImg.style) {
+////    yourImg.style.height = '200px';
+////    yourImg.style.width = '200px';
+////}
 
 
 function pad(n) {
