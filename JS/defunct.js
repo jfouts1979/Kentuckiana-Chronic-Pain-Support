642667d1a56a477541e6a6e6933382ddfc238608
jfouts1979 <Fouts.John@gmail.com>
2015-09-11 10:09:58 -0400

    Made additional changes
    Updated CSS and Directions Pages

--- JS/defunct.js
+++ JS/defunct.js
@@ -156,3 +156,25 @@ function getnumli() {
 //    return (ulw / $('li').length) + 'px';
 //});
 
+window.onload = function() {
+  var startPos;
+  var geoOptions = {
+    enableHighAccuracy: true
+  }
+
+  var geoSuccess = function(position) {
+    startPos = position;
+    document.getElementById('startLat').innerHTML = startPos.coords.latitude;
+    document.getElementById('startLon').innerHTML = startPos.coords.longitude;
+  };
+  var geoError = function(error) {
+    console.log('Error occurred. Error code: ' + error.code);
+    // error.code can be:
+    //   0: unknown error
+    //   1: permission denied
+    //   2: position unavailable (error response from location provider)
+    //   3: timed out
+  };
+
+  navigator.geolocation.getCurrentPosition(geoSuccess, geoError, geoOptions);
+};
